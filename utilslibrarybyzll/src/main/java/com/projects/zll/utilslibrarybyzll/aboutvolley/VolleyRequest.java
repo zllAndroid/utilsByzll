a688e2989543077a839ae808a29c9406df8aabf0
zllAndroid <love8023>
2018-07-26 09:33:25 +0800

    添加聊天界面

--- utilslibrarybyzll/src/main/java/com/projects/zll/utilslibrarybyzll/aboutvolley/VolleyRequest.java
+++ utilslibrarybyzll/src/main/java/com/projects/zll/utilslibrarybyzll/aboutvolley/VolleyRequest.java
@@ -4,9 +4,13 @@ import android.content.Context;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,11 +19,31 @@ public class VolleyRequest {
     public static void RequestGet(final Context context, String url, VolleyInterface volleyInterface) {
         String tag = url;
         VolleyHelper.getInstance(context).cancelPendingRequests(tag);
-        stringRequest = new StringRequest(Request.Method.GET, url, volleyInterface.loadingListener(),
+//        stringRequest = new StringRequest(Request.Method.GET, url, volleyInterface.loadingListener(),
+//                volleyInterface.errorListener());
+        stringRequest = new UTFStringRequest(Request.Method.GET, url, volleyInterface.loadingListener(),
                 volleyInterface.errorListener());
         stringRequest.setRetryPolicy(new DefaultRetryPolicy(50000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         VolleyHelper.getInstance(context).addToRequestQueue(stringRequest, tag);
     }
+    public static class UTFStringRequest extends StringRequest {
+        @Override
+        protected Response<String> parseNetworkResponse(NetworkResponse response) {
+            String parsed;
+
+            try {
+                parsed = new String(response.data, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                parsed = new String(response.data);
+            }
+            return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
+        }
+        public UTFStringRequest(int method, String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+            super(method, url, listener, errorListener);
+
+        }
+    }
+
     public static void MyRequest(final Context context, String url, VolleyResponse response) {
         String tag = url;
         stringRequest = new StringRequest(Request.Method.GET, url,
